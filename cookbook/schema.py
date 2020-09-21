7659f80bdce6a26ff6c76a82f0483e4e1171573d
MarshVishal <vishal.lobo.82@gmail.com>
2020-09-21 09:54:11 +0530

    mutations to create categotry

--- cookbook/schema.py
+++ cookbook/schema.py
@@ -16,13 +16,40 @@ class IngredientType(DjangoObjectType):
         fields = ("id", "name", "notes", "category")
 
 
+# class CategoryInput(graphene.InputObjectType):
+#     name = graphene.String(required=True)
+#     pass
+
+class CreateCategory(graphene.Mutation):
+    ok = graphene.Boolean()
+    status = graphene.String()
+
+    class Input:
+        name = graphene.String(required=True)
+
+    # cat = graphene.Field(lambda: CategoryType)
+
+    @staticmethod
+    def mutate(self, info, name):
+        c = Category.objects.create(name=name)
+        return {"ok": True, "status": "Created successfully"}
+
+
+class Mutation(graphene.ObjectType):
+    create_category = CreateCategory.Field()
+
+
 class Query(graphene.ObjectType):
     all_ingredients = graphene.List(IngredientType)
+    all_categories = graphene.List(CategoryType)
     category_by_name = graphene.Field(CategoryType, name=graphene.String(required=True))
 
     def resolve_all_ingredients(root, info):
         # We can easily optimize query count in the resolve method
         return Ingredient.objects.select_related("category").all()
+
+    def resolve_all_categories(root, info):
+        return Category.objects.all()
 
     def resolve_category_by_name(root, info, name):
         try:
@@ -31,4 +58,4 @@ class Query(graphene.ObjectType):
             return None
 
 
-schema = graphene.Schema(query=Query)
+schema = graphene.Schema(query=Query, mutation=Mutation)
